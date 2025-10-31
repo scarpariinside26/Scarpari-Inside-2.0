f10c74850a4d616288c9ca42ab42e26e630d2c15
scarpariinside26 <scarpariinside@gmail.com>
2025-10-31 16:39:08 +0100

    Create +page.svelte

---
+++ src/routes/match/[eventId]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import MatchVoting from '$lib/MatchVoting.svelte';
+
+    // Ottiene i dati dal file +page.server.js
+    export let data;
+</script>
+
+<div class="match-page">
+    {#if data.roster}
+        <MatchVoting 
+            eventId={data.eventId} 
+            roster={data.roster} 
+            isMatchOwner={data.isMatchOwner}
+        />
+    {:else}
+        <p>Caricamento...</p>
+    {/if}
+</div>

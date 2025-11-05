11595187ee7ecc715e8fc35d565bf39651c5bcb6
scarpariinside26 <scarpariinside@gmail.com>
2025-11-05 16:24:57 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,7 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		// Specifica a SvelteKit di usare l'adattatore Vercel
-		adapter: adapter(),
-		
-        // Opzionale: disabilita il pre-rendering globale se non necessario,
-        // in modo da non avere problemi con le API solo lato server.
-        prerender: {
-			entries: []
-		}
+		adapter: adapter()
 	}
 };
-
-export default config;

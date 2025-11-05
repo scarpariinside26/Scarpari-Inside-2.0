b270911c8b7d102bdb413a19572e84ff7234abcd
scarpariinside26 <scarpariinside@gmail.com>
2025-11-05 16:35:26 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined
-		})
+		adapter: adapter()
 	}
 };

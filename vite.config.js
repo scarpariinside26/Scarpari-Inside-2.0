b670aa651d5e019ff5cff3c182e9ee0de1cea53b
scarpariinside26 <scarpariinside@gmail.com>
2025-11-06 14:21:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+    // Importante: Aggiungi un resolver per il SvelteKit alias $lib
+    resolve: {
+        alias: {
+            '$lib': '/src/lib',
+        }
+    }
 });

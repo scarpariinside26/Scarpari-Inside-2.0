cb2d162a890999adc1e1c507b7af2a441eebc4cb
scarpariinside26 <scarpariinside@gmail.com>
2025-11-06 09:59:18 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import adapter from '@sveltejs/adapter-vercel';
-// Importiamo il preprocessore standalone 'svelte-preprocess', che abbiamo aggiunto in package.json.
-// Questo è il fix per risolvere l'errore 'vitePreprocess' che non viene esportato correttamente
-// nell'ambiente di compilazione di Vercel/SvelteKit.
 import preprocess from 'svelte-preprocess'; 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Utilizziamo il preprocessore esterno per garantire stabilità
 	preprocess: preprocess(), 
 
 	kit: {
 		// Stiamo utilizzando l'adapter Vercel per il deployment
-		adapter: adapter(), 
+		adapter: adapter({
+			// !!! FIX NODE VERSION !!!
+			// Forziamo l'utilizzo di Node 20, supportato da Vercel e SvelteKit.
+			runtime: 'nodejs20.x' 
+		}), 
 	}
 };
 
